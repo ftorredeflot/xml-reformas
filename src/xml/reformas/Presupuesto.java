9072ba067c7c3c9cedb7e07c8be4ad08c52f1f78
ftorredeflot <ftorredeflot@gmail.com>
2017-05-07 18:32:45 +0200

    added pressupuesto

---
+++ src/xml/reformas/Presupuesto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xml.reformas;
+
+import java.io.Serializable;
+/**
+ *
+ * @author ferran
+ */
+public class Presupuesto implements Serializable {
+    
+}
